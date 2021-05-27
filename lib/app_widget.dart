54d064fe988d29c637f433cd8bed460b854460f7
Brandao Developer <dannnbrandao@gmail.com>
2021-05-27 02:09:39 -0300

    ajuste

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:user_form/user_form_view.dart';
+import 'package:user_form/pages/user_form_view.dart';
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
