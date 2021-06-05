d5bfafcedc3edf30602cdf933b353909947467cb
Brandao Developer <dannnbrandao@gmail.com>
2021-06-05 01:16:50 -0300

    error

--- lib/pages/user_form_view.dart
+++ lib/pages/user_form_view.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:user_form/pages/user_form_controller.dart';
 import 'package:user_form/shared/components/text_form_field_component.dart';
-import 'package:user_form/shared/model/user.dart';
 
 class UserForm extends StatelessWidget {
   final _controller = UserFormController();
-  User? user;
+
   @override
   Widget build(BuildContext context) {
     final _size = MediaQuery.of(context).size;
